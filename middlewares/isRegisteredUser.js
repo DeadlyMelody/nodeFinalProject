d3113b02d248e6f1c05edd4f7e6b6a23959fab77
DeadlyMelody <nhhek557@gmail.com>
2025-02-07 13:14:39 +0200

    m

--- middlewares/isRegisteredUser.js
+++ middlewares/isRegisteredUser.js
@@ -5,20 +5,16 @@ import CardSchema from "../cards/models/Card.schema.js";
 export const isRegisteredUser = (isCard) => async (req, res, next) => {
   const card = await CardSchema.findById(req.params.id);
 
-  if (isCard && !card && req.user._id !== card.userId.toString()) {
-    return res
-      .status(403)
-      .json({
-        message:
-          "You are not allowed to perform this action, you are not the authorized user !!!",
-      });
+  if (isCard && !card && req.user._id !== card.userId) {
+    return res.status(403).json({
+      message:
+        "You are not allowed to perform this action, you are not the authorized user !!!",
+    });
   }
   if (!isCard && req.user._id !== req.params.id) {
-    return res
-      .status(403)
-      .json({
-        message:
-          "You are not allowed to perform this action, you are not the authorized user !!!",
-      });
+    return res.status(403).json({
+      message:
+        "You are not allowed to perform this action, you are not the authorized user !!!",
+    });
   } else return next();
 };
